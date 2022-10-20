09382a7fb72afdf28480ed8067ea97b42130de45
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-20 10:44:46 -0300

    step1 full implmentado V1

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -1,10 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react';
  
 export const FormStep1 = () => {
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, []);
 
     const handleNextStep = () => {
-        
+        if(state.name !== ''){
+            navigate('/step2');
+        }else{
+            alert("Preencha seus dados.");
+        }
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
     }
 
     return (
@@ -21,6 +44,8 @@ export const FormStep1 = () => {
                     <input
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
                 <button onClick={handleNextStep}>Próximo</button>
